f1a56001318595203741cfcb1eb1f3e9e84d607d
forge34 <28455546+forge34@users.noreply.github.com>
2024-08-11 15:11:05 +0300

    feat : add login & signup routes

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -2,44 +2,97 @@ import expressAsyncHandler from "express-async-handler";
 import { body, validationResult } from "express-validator";
 import { NextFunction, Request, Response } from "express";
 import passport from "passport";
-import type { User } from "@prisma/client";
+import { PrismaClient, type User } from "@prisma/client";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
-const login = [
-  body("username")
-    .trim()
-    .isLength({ min: 1 })
-    .withMessage("username is too short")
-    .escape(),
-  body("password")
-    .trim()
-    .isLength({ min: 8 })
-    .withMessage("Password should be at least 8 characters")
-    .escape(),
-  expressAsyncHandler(
-    async (req: Request, res: Response, next: NextFunction) => {
-      const errors = validationResult(req);
+const prisma = new PrismaClient();
 
-      if (errors.isEmpty()) {
-        next();
-      } else {
-        res.status(401).json({ errors: errors.array() });
-      }
+class Auth {
+  static signup = [
+    body("username")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("username is too short")
+      .escape(),
+    body("password")
+      .trim()
+      .isLength({ min: 8 })
+      .withMessage("Password should be at least 8 characters")
+      .escape(),
+    body("confirmPassword")
+      .trim()
+      .isLength({ min: 8 })
+      .withMessage("Password should be at least 8 characters")
+      .custom((value, { req }) => {
+        return value === req.body.password;
+      })
+      .escape(),
+
+    expressAsyncHandler(
+      async (req: Request, res: Response, next: NextFunction) => {
+        const errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+          bcrypt.hash(req.body.password, 10, async (err, hash) => {
+            if (!err) {
+              await prisma.user.create({
+                data: {
+                  name: req.body.username,
+                  password: hash,
+                },
+              });
+              res.status(200).json("created user");
+            } else if (err) {
+              console.log(err);
+              res.status(401);
+            }
+          });
+        } else {
+          res.status(401).json({ errors: errors.array() });
+        }
+      },
+    ),
+  ];
+
+  static login = [
+    body("username")
+      .trim()
+      .isLength({ min: 1 })
+      .withMessage("username is too short")
+      .escape(),
+    body("password")
+      .trim()
+      .isLength({ min: 8 })
+      .withMessage("Password should be at least 8 characters")
+      .escape(),
+    expressAsyncHandler(
+      async (req: Request, res: Response, next: NextFunction) => {
+        const errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+          next();
+        } else {
+          res.status(401).json({ errors: errors.array() });
+        }
+      },
+    ),
+    passport.authenticate("local", { session: false }),
+    (req: Request, res: Response) => {
+      const currentUser = req.user as User;
+      const token = jwt.sign({ id: currentUser.id }, process.env.SECRET, {
+        expiresIn: "7d",
+      });
+
+      res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      });
+
+      res.status(200).json("Login sucess");
     },
-  ),
-  passport.authenticate("local"),
-  (req: Request, res: Response) => {
-    const currentUser = req.user as User;
-    const token = jwt.sign({ id: currentUser.id }, process.env.SECRET, {
-      expiresIn: "7d",
-    });
+  ];
+}
 
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-    });
-
-    res.status(200);
-  },
-];
+export default Auth;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from "express";
+import Auth from "../controllers/auth-controller";
 
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
   res.json("index");
 });
+
+router.post("/signup", Auth.signup);
+router.post("/login" ,Auth.login)
 
 export default router;
