827bff33fa71159abfc3de4a26ea352b0e88937a
forge34 <28455546+forge34@users.noreply.github.com>
2024-09-16 15:15:03 +0300

    test : update user test
    add more description blocks
    separate several expect into test block

--- src/tests/user-routes.test.ts
+++ src/tests/user-routes.test.ts
@@ -5,46 +5,86 @@ import { app } from "../app";
 import { tokenString } from "./setup";
 const server = createServer(app);
 
+describe("/users route", () => {
+  describe("/users/me ", () => {
+    describe("given a valid jwt ,", () => {
+      test("returns code 200", async () => {
+        const res = await request(server)
+          .get("/users/me")
+          .set("Cookie", tokenString);
 
+        expect(res.status).toEqual(200);
+      });
 
-describe("user GET routes", () => {
-  test("returns all users except currently logged user", async () => {
-    const res = await request(server).get("/users").set("Cookie", tokenString);
+      test("returns logged user", async () => {
+        const res = await request(server)
+          .get("/users/me")
+          .set("Cookie", tokenString);
 
-    expect(res.headers["content-type"]).toMatch(/json/);
-    expect(res.status).toEqual(200);
-    const resBody = res.body as Array<User>;
-    expect(resBody.some((item) => item.name === "forge2")).toBe(true);
+        const resBody = res.body as User;
+
+        expect(res.status).toEqual(200);
+        expect(resBody.name).toMatch("forge");
+      });
+    });
+
+    describe("given invalid jwt", () => {
+      test("returns 401", async () => {
+        const res = await request(server).get("/users/me");
+
+        expect(res.status).toEqual(401);
+      });
+    });
   });
 
-  test("returns logged user", async () => {
-    const res = await request(server)
-      .get("/users/me")
-      .set("Cookie", tokenString);
+  describe("/users/:userid/block", () => {
+    test("blocks user", async () => {
+      const res = await request(server)
+        .post("/users/cm13qq17b000008l80a935uq7/block")
+        .set("Cookie", tokenString);
 
-    const resBody = res.body as User;
+      expect(res.status).toEqual(200);
 
-    expect(res.status).toEqual(200);
-    expect(resBody.name).toMatch("forge");
+      const res2 = await request(server)
+        .get("/users/me")
+        .set("Cookie", tokenString);
+
+      const user = res2.body as User & { blocked: Array<User> };
+
+      expect(
+        user.blocked.some((blockedUser) => blockedUser.name === "forge2"),
+      ).toBe(true);
+    });
   });
-});
 
-describe("user POST routes", () => {
-  test("blocks user", async () => {
-    const res = await request(server)
-      .post("/users/cm13qq17b000008l80a935uq7/block")
-      .set("Cookie", tokenString);
+  describe("/users", () => {
+    describe("given valid jwt", () => {
+      test("return code 200", async () => {
+        const res = await request(server)
+          .get("/users")
+          .set("Cookie", tokenString);
 
-    expect(res.status).toEqual(200);
+        expect(res.status).toEqual(200);
+      });
+      test("returns all users except currently logged user", async () => {
+        const res = await request(server)
+          .get("/users")
+          .set("Cookie", tokenString);
 
-    const res2 = await request(server)
-      .get("/users/me")
-      .set("Cookie", tokenString);
+        expect(res.status).toEqual(200);
+        const resBody = res.body as Array<User>;
+        expect(resBody.some((item) => item.name === "forge2")).toBe(true);
+      });
+    });
 
-    const user = res2.body as User & { blocked: Array<User> };
+    describe("given invalid jwt", () => {
+      test("returns 404", async () => {
+        const res = await request(server)
+          .get("/users")
+          .set("Cookie", tokenString);
 
-    expect(
-      user.blocked.some((blockedUser) => blockedUser.name === "forge2"),
-    ).toBe(true);
+        expect(res.status).toEqual(200);
+      });
+    });
   });
 });
